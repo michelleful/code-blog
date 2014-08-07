6686a626cb3d9f01a2e4e70278e8471b2e04cbd8
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-08-07 15:42:55 -0400

    Got first working instance of checkbox grading going

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -7,45 +7,61 @@ if(typeof(String.prototype.trim) === "undefined")
     };
 }
 
+// add equals to check for equality between two jQuery collections
+$.fn.equals = function(compareTo) {
+  if (!compareTo || this.length != compareTo.length) {
+    return false;
+  }
+  for (var i = 0; i < this.length; ++i) {
+    if (this[i] !== compareTo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 // functions for verifying whether an answer is correct (true) or wrong (false)
-function isTextCorrect(elt) {
+function gradeText(elt) {
     if (elt.val().trim().toLowerCase() == elt.attr("answer")) {
-        return true
+        markCorrect(elt);
     } else {
-        return false
+        markWrong(elt);
     }
 }
 
-function isCheckboxCorrect(elt) {
-    
+function gradeCheckbox(form) {
+    if (form.children("[value='correct']").equals(form.children(":checked"))) {
+        markCorrect(form.children().first());
+    } else {
+        markWrong(form.children().first());
+    }
 }
-
-// TODO: functions for checkboxes and radio buttons
 
 // functions for marking an answer right or wrong
 function markCorrect(elt) {
     // mark it with a green checkmark
     elt.siblings(".result").html("&#10003;").addClass("big").css("color","green");
-    // put discussion text in hint div
+    // put discussion text in hint div (if any)
     elt.siblings(".hint").html(elt.attr("discuss"));
 };
 
 function markWrong(elt) {
     // mark it with a red cross
     elt.siblings(".result").html("&#x2717;").addClass("big").css("color","red");
-    // put hint text in hint div
+    // put hint text in hint div (if any)
     elt.siblings(".hint").html("Hint: " + elt.attr("hint"))
 };
 
 $(document).ready(function() {
 
     $("form.textType").submit(function() {
-       var answerElt = $(this).children(":text");
-       if(isTextCorrect(answerElt)) {
-           markCorrect(answerElt);
-       } else {
-           markWrong(answerElt);
-       };
-       return false;
+        var answerElt = $(this).children(":text");
+        gradeText(answerElt);
+        return false; // prevent form from submitting
+    });
+
+    $("form.checkboxType").submit(function(event) {
+        gradeCheckbox($(this));
+        return false; // prevent form from submitting
     });
 });
